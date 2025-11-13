a42d626b13c14109720e46e3c4e8126a6c7825c0
Mohammed Saqlain Altaf <72072526+Saqlainaltaf@users.noreply.github.com>
2025-11-13 20:38:55 +0530

    Import Bootstrap JavaScript bundle in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
+// top of src/index.js (or where you import global CSS)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./App";
 
 
